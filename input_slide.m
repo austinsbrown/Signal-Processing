f10acc3bba9b0b628dba277e4b3f5d401ecdf185
AUstin Brown <austinsbrown@gmail.com>
2021-07-19 18:24:05 -0500

    removed some stuff

--- input_slide.m
+++
@@ -1,9 +0,0 @@
-x = 0:.1:10;
-h = [2,4,1,2];
-y = zeros(1,length(x) + length(h));
-
-for i = 1:length(x)
-    for j = 1:length(h)
-        y(i+j) = y(i+j) + x(i) * h(j);
-    end
-end
--- running_sum.m
+++
@@ -1,6 +0,0 @@
-x = 0:.01:10;
-y = x(1);
-
-for i = 2:length(x)
-    y(i) = y(i-1) + x(i);
-end
